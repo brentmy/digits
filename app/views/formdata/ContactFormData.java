f7088aea293c9f6cd917946f20d3af41f00b1d05
brentmy <brentmy@gmail.com>
2013-10-13 11:12:18 -1000

    check

---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,15 @@
+package views.formdata;
+/**
+ * Backing class for form data.
+ * @author Brent
+ *
+ */
+public class ContactFormData {
+  /** the F. */
+public String firstName = "";
+/** the F. */
+public String lastName = "";
+/** the F. */
+public String telephone = "";
+
+}
