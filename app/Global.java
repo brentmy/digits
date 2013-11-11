a70f05b47c8674011c211613c95507e55e1aaa3a
brentmy <brentmy@gmail.com>
2013-11-11 13:51:57 -1000

    Finished

--- app/Global.java
+++ app/Global.java
@@ -2,6 +2,7 @@
  * 
  */
 import models.ContactDB;
+import models.UserInfoDB;
 import play.Application;
 import play.GlobalSettings;
 import views.formdata.ContactFormData;
@@ -15,6 +16,7 @@ public class Global extends  GlobalSettings {
    * @param app is app.
    */
   public void onStart(Application app) {
+    UserInfoDB.addUserInfo("Brent Yoshida", "brent@gmail.com", "password");
     ContactDB.addContact(new ContactFormData("Brent", "Yosh1", "929-999-9999", "Home"));
     ContactDB.addContact(new ContactFormData("Brent1", "Yosh2", "939-999-9999", "Mobile"));
     ContactDB.addContact(new ContactFormData("Brent2", "Yosh3", "949-999-9999", "Work"));
